50d301dcb66662aa9c6cfd25c6d882839e036e15
yabeen0126 <lhb0126@gmail.com>
2024-10-03 15:34:11 +0900

    서보모터 2개 동시작동

--- servo_test1.ino
+++ servo_test1.ino
@@ -3,17 +3,21 @@
 Servo servo1;
 Servo servo2;  
 int servoPin1 = D6;
+int servoPin2 = D7;
 
 void setup() {
   Serial.begin(115200);
   servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
 }
 
 void loop() {
    servo1.write(180);
+   servo2.write(180);
    delay(2000);
   
    servo1.write(0);
+   servo2.write(0);
    delay(2000);
 
 }
