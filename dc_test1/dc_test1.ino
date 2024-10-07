2b64e698a22103aea8574ae7d84d571f1df92086
yabeen0126 <lhb0126@gmail.com>
2024-10-07 13:03:06 +0900

    motorBackward, 모터 속도 조절

--- dc_test1/dc_test1.ino
+++ dc_test1/dc_test1.ino
@@ -1,29 +1,28 @@
-// int AA = D8;               // 모터 A의 A 핀
-// int AB = D9;               // 모터 A의 B 핀
-int BA = D6;               // 모터 B의 A 핀
-int BB = D7;               // 모터 B의 B 핀
-int STATUS = 0;
+int IN1 = D6;  // 모터 A의 IN1 핀 (PWM 가능 핀)
+int IN2 = D7;  // 모터 A의 IN2 핀 (정방향/역방향 제어)
+int speedValue = 100;  // 모터 속도 (0~255 사이 값)
 
-void setup() 
-{
-  // pinMode(AA, OUTPUT);
-  // pinMode(AB, OUTPUT);
-  pinMode(BA, OUTPUT);
-  pinMode(BB, OUTPUT);    
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
 }
 
-void loop() 
-{
-  
-  motorForward();           // 모터를 정방향으로 회전
-  delay(5000);              // 5초간 대기
+void loop() {
+  motorForward();  // 모터를 정방향으로 회전
+  delay(5000);     // 5초간 회전
+
+  motorBackward(); // 모터를 역방향으로 회전
+  delay(5000);     // 5초간 회전
 }
 
 // 모터를 정방향으로 회전시키는 함수
-void motorForward() 
-{
-  // digitalWrite(AA, HIGH);   // 모터 A 정방향
-  // digitalWrite(AB, LOW);    
-  digitalWrite(BA, HIGH);   // 모터 B 정방향
-  digitalWrite(BB, LOW);
+void motorForward() {
+  analogWrite(IN1, speedValue);  // PWM 신호로 속도 제어
+  digitalWrite(IN2, LOW);        // 정방향 회전
+}
+
+// 모터를 역방향으로 회전시키는 함수
+void motorBackward() {
+  analogWrite(IN1, speedValue);  // PWM 신호로 속도 제어
+  digitalWrite(IN2, HIGH);       // 역방향 회전
 }
